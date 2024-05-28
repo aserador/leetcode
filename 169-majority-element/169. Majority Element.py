6a7bb61d5abf882c8da1c805e0e696c09c38a8c4
Audric Serador <108915409+aserador@users.noreply.github.com>
2024-05-28 02:32:37 -0500

    Runtime: 173 ms (46.55%), Memory: 18 MB (33.83%) - Gitcode

--- 169-majority-element/169. Majority Element.py
+++ 169-majority-element/169. Majority Element.py
@@ -1,6 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counter = collections.defaultdict(int)
+        hashmap = collections.defaultdict(int)
         for n in nums:
-            counter[n] += 1
-        return list(counter.keys())[list(counter.values()).index(max(counter.values()))]
+            hashmap[n] += 1
+        target = math.floor(len(nums) / 2)
+        for n, count in hashmap.items():
+            if count > target:
+                return n
+            
+        
