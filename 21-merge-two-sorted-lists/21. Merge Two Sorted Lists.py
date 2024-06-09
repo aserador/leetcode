b2de823868e9e997bb6bb6de22483486b6b0dfb7
Audric Serador <108915409+aserador@users.noreply.github.com>
2024-06-09 00:19:31 -0500

    Runtime: 61 ms (6.19%), Memory: 16.4 MB (88.94%) - Gitcode

--- 21-merge-two-sorted-lists/21. Merge Two Sorted Lists.py
+++ 21-merge-two-sorted-lists/21. Merge Two Sorted Lists.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        ans = ListNode(-999999999)
+        ans = ListNode()
         curr = ans
         while list1 and list2:
             if list1.val <= list2.val:
